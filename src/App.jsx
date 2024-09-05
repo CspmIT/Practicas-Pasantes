c5c3afffc3782d1f791cb836f8d783d11fe537df
JonaMaidana <jonamaidana18l@gmail.com>
2024-09-05 14:15:47 -0300

    NavModificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Formulario from './modules/Formulario/views/Index'
 import BaseInitial from './modules/BaseInitial/Index'
-
+import Navbar from './components/Navbar/Index'
 
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         {rutas.map((route) => (
           <Route path={route.path} element={route.element} />
--- src/modules/BaseInitial/Index.jsx
+++ src/modules/BaseInitial/Index.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ButtonUser from '../../components/ButtonUser/Index'
-import Navbar from '../../components/Navbar/Index';
-
 
 function Index() {
     return (
         <div>
-            <div>
-                <Navbar />
-            </div>
             <main>
                 <h1>Bienvenido a la aplicación</h1>
                 <p>Este es el contenido de tu aplicación.</p>
